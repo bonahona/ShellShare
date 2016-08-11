c7f6159c6275df5d92c31adfd8ef4247c384e969
Bona <bjorn.fyrvall@gmail.com>
2016-08-12 00:52:45 +0200

    Start of global postback based folder management.

--- Application/Controllers/BaseController.php
+++ Application/Controllers/BaseController.php
@@ -1,9 +1,20 @@
 <?php
 class BaseController extends Controller
 {
+    public $RootDirectories;
+    public $VirtualDirectories;
+
     public function BeforeAction()
     {
-        $rootDirectories = $this->Models->VirtualDirectory->Where(array('ParentDirectoryId' => null));
-        $this->Set('RootDirectories', $rootDirectories);
+        $this->RootDirectories = $this->Models->VirtualDirectory->Where(array('ParentDirectoryId' => null));
+        $this->Set('RootDirectories', $this->RootDirectories);
+
+        $this->VirtualDirectories = $this->Models->VirtualDirectory->All();
+        $this->Set('VirtualDirectories', $this->VirtualDirectories);
+    }
+
+    protected function IsFolderOpen($folderPath)
+    {
+        return true;
     }
 }
