8a3474014ae147b878bf096bae18dd71e8763254
Björn Fyrvall <bjorn.fyrvall@gmail.com>
2016-08-30 16:56:44 +0200

    More css fixes for the sidebar.

--- Application/Views/Partial/Sidebar.php
+++ Application/Views/Partial/Sidebar.php
@@ -4,14 +4,16 @@ function PrintBranch($directories, $controller)
     foreach($directories as $directory){
 ?>
     <li folder-link="/<?php echo $directory->GetFullPath();?>">
-        <?php if($controller->IsFolderOpen($directory->Id)):?>
-            <i class="indicator glyphicon glyphicon-folder-open"></i>
-        <?php else:?>
-            <i class="indicator glyphicon glyphicon-folder-close"></i>
-        <?php endif;?>
-        <a href="<?php echo $directory->GetLinkPath();?>">
-            <?php echo $directory->Name;?>
-        </a>
+        <span class="text-nowrap header">
+            <?php if($controller->IsFolderOpen($directory->Id)):?>
+                <i class="indicator glyphicon glyphicon-folder-open"></i>
+            <?php else:?>
+                <i class="indicator glyphicon glyphicon-folder-close"></i>
+            <?php endif;?>
+            <a href="<?php echo $directory->GetLinkPath();?>">
+                <?php echo $directory->Name;?>
+            </a>
+        </span>
         <?php if($controller->IsFolderOpen($directory->Id)):?>
             <ul>
                 <?php PrintBranch($directory->VirtualDirectories, $controller);?>
@@ -30,8 +32,10 @@ function PrintBranch($directories, $controller)
             <div class="col-lg-12">
                 <ul id="SideBarTree" class="tree">
                     <li folder-link="/" class="branch">
-                        <i class="indicator glyphicon glyphicon-folder-open"></i>
-                        <a href="/Files/Details/"><?php echo $this->Html->SafeHtml('<root>');?></a>
+                        <span>
+                            <i class="indicator glyphicon glyphicon-folder-open"></i>
+                            <a href="/Files/Details/"><?php echo $this->Html->SafeHtml('<root>');?></a>
+                        </span>
                         <ul>
                             <li>
                                 <?php PrintBranch($RootDirectories, $this);?>
